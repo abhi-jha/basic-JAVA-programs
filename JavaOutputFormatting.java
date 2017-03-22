ca66f4ca002ee7987a30a093ca35db35becb6749
abhi-jha <abhi_jha1995@rediffmail.com>
2017-03-22 05:23:35 -0700

    Java Output Formatting

--- JavaOutputFormatting.java
+++ JavaOutputFormatting.java
@@ -10,7 +10,6 @@ public class JavaOutputFormatting {
                 String s1=sc.next();
                 int x=sc.nextInt();
                 System.out.printf("%-15s%03d%n",s1, x);
-                
             }
             System.out.println("================================");
 
