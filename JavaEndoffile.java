03f92d9a34cdfb27434556fc1e1567835283273c
abhi-jha <abhi_jha1995@rediffmail.com>
2017-03-22 05:30:27 -0700

    Java End-of-file

---
+++ JavaEndoffile.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaEndoffile {
+
+    public static void main(String[] args) {
+       
+        Scanner scan = new Scanner(System.in);
+        int i = 1;
+        while(scan.hasNext()){
+            System.out.println(i++ +" "+scan.nextLine());
+        }
+    }
+}
