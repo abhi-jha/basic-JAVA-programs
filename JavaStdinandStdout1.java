a3d44d5caf2effb58e0521a698c2fed0d648fc15
abhi-jha <abhi_jha1995@rediffmail.com>
2017-03-22 05:20:57 -0700

    Java Stdin and Stdout 1

--- JavaStdinandStdout1.java
+++ JavaStdinandStdout1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class JavaStdinandStdout {
+public class JavaStdinandStdout1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
